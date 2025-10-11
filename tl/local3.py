f4854d25e0fc71444903d16b90733d439b7eb707
armadi9 <45158825+armadi9@users.noreply.github.com>
2025-10-11 21:36:29 +0800

    Update local3.py

--- tl/local3.py
+++ tl/local3.py
@@ -54,7 +54,7 @@ closed_tabs_count = 0
 # ----------------------------
 # Limit jumlah window (maksimal 4 sekaligus)
 # ----------------------------
-window_limit = asyncio.Semaphore(2)
+window_limit = asyncio.Semaphore(3)
 
 async def get_browser():
     global browser
@@ -622,6 +622,7 @@ async def status():
 # ----------------------------
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8090)
+
 
 
 
